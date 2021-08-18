1214c468e73daa639ae859c09bbb9db8ffc02b2e
Milad Sadeghi <miladsadeghi2323@gmail.com>
2021-08-18 22:44:30 +0430

    select score span, and change something in head of snake

--- Assest/JS/main.js
+++ Assest/JS/main.js
@@ -1,21 +1,25 @@
-const canvas = document.getElementById('canvas')
-const ctx = canvas.getContext('2d')
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-let box = 32
+const scoreSpan = document.querySelector("#score");
 
-const snakeHead = new Image()
-snakeHead.src = '/Assest/Images/snakeHead.png'
+let box = 32;
+let direction;
+let score = 0;
 
-const foodImg = new Image()
-foodImg.src = '/Assest/Images/apple.png'
+const snakeHead = new Image();
+snakeHead.src = "/Assest/Images/snakeHead.png";
+
+const foodImg = new Image();
+foodImg.src = "/Assest/Images/apple.png";
 
 let food = {
   x: Math.floor(Math.random() * 40) * box,
   y: Math.floor(Math.random() * 17) * box,
-}
+};
 
-let snake = []
+let snake = [];
 snake[0] = {
-  x:(canvas.clientWidth / 64) * box,
-  y:(canvas.clientHeight / 64) * box
-}
+  x: 19 * box,
+  y: 8 * box,
+};
