a2e19e825808ec1cf4c537584a0c2b4fafbf1152
Milad Sadeghi <miladsadeghi2323@gmail.com>
2021-08-18 16:34:01 +0430

    create snake as array, and fill first member of array as location of middle display

--- Assest/JS/main.js
+++ Assest/JS/main.js
@@ -3,10 +3,19 @@ const ctx = canvas.getContext('2d')
 
 let box = 32
 
+const snakeHead = new Image()
+snakeHead.src = '/Assest/Images/snakeHead.png'
+
 const foodImg = new Image()
 foodImg.src = '/Assest/Images/apple.png'
 
 let food = {
   x: Math.floor(Math.random() * 40) * box,
   y: Math.floor(Math.random() * 17) * box,
+}
+
+let snake = []
+snake[0] = {
+  x:(canvas.clientWidth / 64) * box,
+  y:(canvas.clientHeight / 64) * box
 }
