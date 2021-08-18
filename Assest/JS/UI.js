d2b2fa79501266f61669f012ac150941168d4895
Milad Sadeghi <miladsadeghi2323@gmail.com>
2021-08-18 16:50:43 +0430

    add event listener for snake moves

--- Assest/JS/UI.js
+++ Assest/JS/UI.js
@@ -26,6 +26,31 @@ function draw() {
   ctx.drawImage(snakeHead, snake[0].x, snake[0].y, 32, 32)
 
   ctx.drawImage(foodImg, food.x, food.y, 32, 32)
+
+  let snakeX = snake[0].x
+let snakeY = snake[0].y
+
+document.addEventListener('keydown', (e) => {
+  if (e.keyCode === 37 && direction !== 'RIGHT') {
+    direction = 'LEFT'
+  } else if (e.keyCode === 38 && direction !== 'DOWN') {
+    direction = 'UP'
+  } else if (e.keyCode === 39 && direction !== 'LEFT') {
+    direction = 'RIGHT'
+  } else if (e.keyCode === 40 && direction !== 'UP') {
+    direction = 'DOWN'
+  }
+})
+
+if(direction === 'LEFT') snakeX -= box
+if(direction === 'UP') snakeY -= box
+if(direction === 'RIGHT') snakeX += box
+if(direction === 'DOWN') snakeY += box
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
 }
 
 const game = setInterval(draw, 1000)
