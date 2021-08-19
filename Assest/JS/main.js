38925e38f02403158e16cd09e8b7a41ea726d1c9
Milad Sadeghi <miladsadeghi2323@gmail.com>
2021-08-19 15:54:05 +0430

    make it full responsive, now food and snakeHead location, change dynamic Based on display resulution

--- Assest/JS/main.js
+++ Assest/JS/main.js
@@ -5,7 +5,14 @@ const scoreSpan = document.querySelector("#score");
 
 let box = 32;
 let direction;
+let snake = [];
 let score = 0;
+let maxWidth = 0;
+let maxHeight = 0;
+
+const maingame = document.querySelector(".main-game");
+const userWidth = maingame.offsetWidth;
+const userHeight = maingame.offsetHeight;
 
 const snakeHead = new Image();
 snakeHead.src = "/Assest/Images/snakeHead.png";
@@ -13,13 +20,29 @@ snakeHead.src = "/Assest/Images/snakeHead.png";
 const foodImg = new Image();
 foodImg.src = "/Assest/Images/apple.png";
 
-let food = {
-  x: Math.floor(Math.random() * 40) * box,
-  y: Math.floor(Math.random() * 17) * box,
+for (let i = userWidth; i >= 0; i--) {
+  if ((i / 32) % 2 == 0) {
+    maxWidth = i - 32;
+    break;
+  }
+}
+
+for (let i = userHeight; i >= 0; i--) {
+  if ((i / 32) % 2 == 0) {
+    maxHeight = i - 32;
+    break;
+  }
+}
+
+snake[0] = {
+  x: Math.floor(maxWidth / 2 / 32) * box,
+  y: Math.floor(maxHeight / 2 / 32) * box,
 };
 
-let snake = [];
-snake[0] = {
-  x: 19 * box,
-  y: 8 * box,
+let food = {
+  x: Math.floor(Math.random() * (maxWidth / 32)) * box,
+  y: Math.floor(Math.random() * (maxHeight / 32)) * box,
 };
+
+canvas.setAttribute("width", maxWidth);
+canvas.setAttribute("height", maxHeight);
